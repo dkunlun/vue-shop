de89682b67fa413b917cc31f446c061249a36302
dkunlun <519395527@qq.com>
2017-08-15 14:45:22 +0800

    add cart

--- src/common/header.vue
+++ src/common/header.vue
@@ -19,7 +19,8 @@
 								<router-link to="user">个人中心</router-link>
 							</div>
 							<div class="shop pr">
-								<router-link></router-link>
+								<router-link to="cart"></router-link>
+								<span class="cart-num"><i class="no">1</i></span>
 							</div>
 						</div>
 					</div>
@@ -106,6 +107,65 @@
 					background-size: 240px 107px;
 					@include wh(20px);
 					transition: none;
+				}
+			}
+		}
+		.shop {
+			position: relative;
+			float: left;
+			width: 61px;
+			margin-left: 21px;
+			z-index: 99;
+			&:hover {
+				a:before {
+					content: " ";
+					background-position: 0 -22px;
+				}
+			}
+			> a {
+				position: absolute;
+				left: 0;
+				bottom: 0;
+				right: 0;
+				top: 0;
+				display: block;
+				z-index: 1;
+				&:before {
+					display: block;
+					@include wh(30px, 100%);
+					content: " ";
+					background: url(/static/images/account-icon@2x.32d87deb02b3d1c3cc5bcff0c26314ac.png) 0 -22px;
+					background-size: 240px 107px;
+					background-position: -150px -22px;
+				}
+			}
+		}
+		.cart-num {
+			position: relative;
+			display: block;
+			margin-left: 31px;
+			margin-top: -1px;
+			min-width: 30px;
+			text-indent: 0;
+			line-height: 20px;
+			> i {
+				background: #eb746b;
+				background-image: -webkit-linear-gradient(#eb746b, #e25147);
+				background-image: linear-gradient(#eb746b, #e25147);
+				box-shadow: inset 0 0 1px hsla(0, 0%, 100%, .15), 0 1px 2px hsla(0, 0%, 100%, .15);
+				text-align: center;
+				font-style: normal;
+				display: inline-block;
+				@include wh(20px);
+				line-height: 20px;
+				border-radius: 10px;
+				color: #fff;
+				font-size: 12px;
+				&.no {
+					background: #969696;
+					background-image: -webkit-linear-gradient(#A4A4A4, #909090);
+					background-image: linear-gradient(#A4A4A4, #909090);
+					box-shadow: inset 0 0 1px #838383, 0 1px 2px #838383;
 				}
 			}
 		}
